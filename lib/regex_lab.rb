1668670b84ebebb0f3ee12c0fa5135ab533d8b64
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-01 19:32:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,15 +1,16 @@
 require 'pry'
 def starts_with_a_vowel?(word)
-  # binding.pry
 word.match? (/\b[AEIOUaeiou][a-z]*\b/)
-# (var > 3 ? true : false)
+
 end
 
-# def words_starting_with_un_and_ending_with_ing(text)
- # text.scan(/)
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan (/un[a-z]*ing/)
+
+end
 
 # def words_five_letters_long(text)
-#
+
 #   # end
 # end
 #
