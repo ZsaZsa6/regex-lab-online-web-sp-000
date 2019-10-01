6ea8c801e5fdfc77c25c91fcaa2c125b80f379d4
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-01 21:10:47 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,9 +13,10 @@ def words_five_letters_long(text)
 text.scan (/\b\w{5}\b/)
 end
 
-# def first_word_capitalized_and_ends_with_punctuation?(text)
-#
-# end
+def first_word_capitalized_and_ends_with_punctuation?(text)
+  text.match? (/\b^[A-Z]\b/)
+  text.match? (/\w[^A-Za-z_\-]\w/)
+end
 #
 # def valid_phone_number?(phone)
 #
