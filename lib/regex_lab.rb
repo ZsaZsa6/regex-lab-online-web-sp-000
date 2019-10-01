2fd7c182c151f138177577246a6c254a99ca52f5
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-01 14:08:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,14 +8,14 @@ end
  # text.scan(/)
 
 # def words_five_letters_long(text)
-# 
+#
 #   # end
 # end
-# 
+#
 # def first_word_capitalized_and_ends_with_punctuation?(text)
-# 
+#
 # end
-# 
+#
 # def valid_phone_number?(phone)
-# 
+#
 # end
