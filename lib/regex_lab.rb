4bf03c64d39bde6937315ac2b3e717e3c9684f80
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-01 14:08:53 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,18 +4,18 @@ def starts_with_a_vowel?(word)
 word.match (/\b[AEIOUaeiou][a-z]*\b/)
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
- text.scan(/)
-end
+# def words_starting_with_un_and_ending_with_ing(text)
+ # text.scan(/)
 
-def words_five_letters_long(text)
-
-end
-
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
-
-def valid_phone_number?(phone)
-
-end
+# def words_five_letters_long(text)
+# 
+#   # end
+# end
+# 
+# def first_word_capitalized_and_ends_with_punctuation?(text)
+# 
+# end
+# 
+# def valid_phone_number?(phone)
+# 
+# end
